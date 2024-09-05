6a8acc6197bae89b9a510d5300304acf16b6b15f
rodanAsh <aakashash2002@gmail.com>
2024-09-05 11:58:30 +0530

    implememnted routing feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './pages/Login/Login'
+import Chat from './pages/Chat/Chat'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 
 function App() {
 
   return (
-    <div className="app">
-      App
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/profile' element={<ProfileUpdate />} />
+      </Routes>
+    </>
   )
 }
 
