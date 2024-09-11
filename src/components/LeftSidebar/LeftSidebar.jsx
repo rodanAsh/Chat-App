658843cb63bb00a597dace312c980f3941e0d64b
rodanAsh <aakashash2002@gmail.com>
2024-09-11 21:33:06 +0530

    built leftsidebar section for chat page

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './LeftSidebar.css'
+import assets from '../../assets/assets.js'
+
+const LeftSidebar = () => {
+  return (
+    <div className='ls'>
+        <div className="ls-top">
+            <div className="ls-nav">
+                <img src={assets.logo} alt="logo" className='logo' />
+                <div className="menu">
+                    <img src={assets.menu_icon} alt="" />
+                </div>
+            </div>
+            <div className="ls-search">
+                <img src={assets.search_icon} alt="" />
+                <input type="text" placeholder='Search here..' />
+            </div>
+        </div>
+        <div className="ls-list">
+            {
+                Array(12).fill("").map((_,index) => (
+                    <div key={index} className="friends">
+                        <img src={assets.profile_img} alt="" />
+                        <div>
+                            <p>Hari</p>
+                            <span>Hello, How are you?</span>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default LeftSidebar
