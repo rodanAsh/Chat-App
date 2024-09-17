3063f1579a59fcc5b9299c96693c73f3071e1a27
rodanAsh <aakashash2002@gmail.com>
2024-09-17 19:36:33 +0530

    designed chat page ui

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './ChatBox.css'
+import assets from '../../assets/assets.js'
 
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
+    <div className='chat-box'>
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
+        <img src={assets.help_icon} alt="" />
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder='Send a message' />
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className='msg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30pm</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img src={assets.pic1} className='msg-img' alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30pm</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className='msg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30pm</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
