670c338351bfbe6152e333da7b6794493528ef43
rodanAsh <aakashash2002@gmail.com>
2024-09-18 20:05:51 +0530

    implement form handling for authentication using firebase

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
+import { signup } from '../../config/firebase'
 const Login = () => {
   const [currentState,setCurrentState] = useState("Sign Up")
+  const [userName,setUserName] = useState('')
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+
+    if(currentState === "Sign Up"){
+      signup(userName,email,password)
+    }
+
+    // setUserName('')
+    // setEmail('')
+    // setPassword('')
+  }
 
   return (  
     <div className='login'>
       <img src={assets.logo_big} alt="" className="logo" />
-      <form className='login-form'>
+      <form onSubmit={onSubmitHandler} className='login-form'>
         <h2>{currentState}</h2>
-        {currentState === "Sign Up" ? <input type="text" placeholder='username' className="form-input" required /> : null}
-        <input type="email" placeholder='email address' className="form-input" required/>
-        <input type="password" placeholder='password' className="form-input" required/>
+        {currentState === "Sign Up" ? <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='username' className="form-input" required /> : null}
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email address' className="form-input" required/>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' className="form-input" required/>
         <button type='submit'>{currentState === "Sign Up" ? "Sign Up" : "Login Now"}</button>
         <div className="login-term">
           <input type="checkbox" />
